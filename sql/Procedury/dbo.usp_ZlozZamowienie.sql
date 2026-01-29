582f9ce7fe358ead51072dd4212f6c247efd2b95
Stachu1203 <qbastaszel@gmail.com>
2026-01-29 18:33:05 +0100

    Poprawki w procedurze do dodawania zamówienia

--- sql/Procedury/dbo.usp_ZlozZamowienie.sql
+++ sql/Procedury/dbo.usp_ZlozZamowienie.sql
@@ -1,3 +1,6 @@
+USE [u_staszel];
+GO
+
 CREATE OR ALTER PROCEDURE dbo.usp_ZlozZamowienie
     @KlientId INT,
     @ProduktId INT,
@@ -12,19 +15,30 @@ BEGIN
         DECLARE @ZamowienieId INT;
         DECLARE @Dostepne INT;
         DECLARE @Cena DECIMAL(18,2);
-
         
+        DECLARE @WylosowanyPrzewoznik INT;
+        DECLARE @WylosowanyDeadline DATETIME;
+
+        SELECT TOP 1 @WylosowanyPrzewoznik = PrzewoznikID 
+        FROM Przewoznicy_Kurierzy 
+        ORDER BY NEWID();
+
+        SET @WylosowanyDeadline = DATEADD(day, (ABS(CHECKSUM(NEWID())) % 15) + 7, GETDATE());
+
+  
         SELECT @Dostepne = ISNULL(IloscDostepna, 0) FROM Magazyn WHERE ProduktID = @ProduktId;
         SELECT @Cena = CenaSprzedazy FROM Produkt WHERE ProduktID = @ProduktId;
 
-        
-        INSERT INTO Zamowienia (KlientID, DataZamowienia, StatusZamowieniaID, AdresZamowienia)
-        VALUES (@KlientId, GETDATE(), 1, @AdresDostawy);
+    
+        INSERT INTO Zamowienia (KlientID, DataZamowienia, Deadline, StatusZamowieniaID, AdresZamowienia, PrzewoznikID)
+        VALUES (@KlientId, GETDATE(), @WylosowanyDeadline, 1, @AdresDostawy, @WylosowanyPrzewoznik);
         
         SET @ZamowienieId = SCOPE_IDENTITY();
 
+        
         IF @Dostepne >= @Ilosc
         BEGIN
+            
             UPDATE Magazyn SET IloscDostepna = IloscDostepna - @Ilosc WHERE ProduktID = @ProduktId;
             
             INSERT INTO PozycjeZamowienia (ZamowienieID, ProduktID, Ilosc, StatusProdukcjiID, Cena, DataZakonczenia)
@@ -32,11 +46,14 @@ BEGIN
         END
         ELSE
         BEGIN
+            
             INSERT INTO PozycjeZamowienia (ZamowienieID, ProduktID, Ilosc, StatusProdukcjiID, Cena, DataRozpoczecia)
             VALUES (@ZamowienieId, @ProduktId, @Ilosc, 1, @Cena, GETDATE());
         END
 
         COMMIT TRANSACTION;
+        PRINT 'Sukces: Zamówienie złożone. Przewoźnik i termin zostały przydzielone automatycznie.';
+
     END TRY
     BEGIN CATCH
         IF @@TRANCOUNT > 0 ROLLBACK TRANSACTION;
